31169c3930d1922b97d2a846f736c2205760638c
Lukas Böcker <lukas.boecker@provinzial.de>
2024-12-05 09:10:11 +0100

    creates erzeugt

--- rest/src/main/java/de/provinzial/schulung/rest/BuecherConroller.java
+++ rest/src/main/java/de/provinzial/schulung/rest/BuecherConroller.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +35,11 @@ public class BuecherConroller {
 		Optional<BuchDTO> buch = this.buchService.getBuch(id);
 
 		return buch.get();
+	}
+
+	@PostMapping(consumes = "application/json")
+	public BuchDTO createBuch(@RequestBody BuchDTO buch) {
+		return this.buchService.createBuch(buch);
 	}
 
 }
